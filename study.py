60fecb718b650b4d4b0512129f1e0abea1de8774
sumini0516 <sumini0516@catholic.ac.kr>
2022-12-22 15:37:30 +0900

    1222_V2

--- study.py
+++ study.py
@@ -45,4 +45,30 @@ class SelfAttention(nn.Module):
 
 
 class TransformerBlock(nn.Module):
-    def __init__(self):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(TransformerBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion * embed_size),
+            nn.Relu(),
+            nn.Linear(forward_expansion * embed_size, embed_size)
+        )
+        # don't change anything just extra computation and then mapping it back
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, value, key, query, mask):
+        attention = self.attention(value, key, query, mask)
+        x = self.dropout(self.norm1(attention + query))  # skip connection
+        forward = self.feed_forward(x)
+        out = self.dropout(self.norm2(forward + x))
+        return out
+
+class Encoder(nn.Module):
+    def __init__(self, src_vocab_size, embed_size, num_layers, heads, device, forward_expansion, dropout, max_length):
+        super(Encoder, self).__init__()
+        self.embed_size = embed_size
+
